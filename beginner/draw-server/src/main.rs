c86a500d5e07d72ec24c8a147b7c34b120b5dffb
James Munns <james.munns@ferrous-systems.com>
2019-04-14 12:01:47 +0200

    Split out modem comms

--- beginner/draw-server/src/main.rs
+++ beginner/draw-server/src/main.rs
@@ -1,88 +1,27 @@
 use std::time::Duration;
 
 use serialport::prelude::*;
-use postcard::from_bytes;
-use nrf52_bin_logger::LogOnLine;
-use cobs::CobsDecoder;
-use protocol::ModemUartMessages;
+use std::sync::mpsc::channel;
+
+
+use modem_comms::modem_task;
+
+mod modem_comms;
 
 fn main() {
     let mut settings: SerialPortSettings = Default::default();
     settings.timeout = Duration::from_millis(10);
     settings.baud_rate = 115200;
 
+    let (prod_cmds, cons_cmds) = channel();
+
     match serialport::open_with_settings("/dev/ttyACM0", &settings) {
-        Ok(mut port) => {
-            let mut cobs_buf: Vec<u8> = vec![0; 2048];
-            let mut cobs_dec = CobsDecoder::new(cobs_buf.as_mut_slice());
-            println!("Receiving data on {} at {} baud:", "/dev/ttyACM0", "115200");
-
-            loop {
-                let mut buf = [0u8; 1024];
-                match port.read(&mut buf) {
-                    Ok(t) => {
-                        let mut pos = 0;
-                        'inner: loop {
-                            match cobs_dec.push(&buf[pos..t]) {
-                                Ok(Some((n, m))) => {
-                                    match from_bytes::<LogOnLine<ModemUartMessages>>(&cobs_buf[..n]) {
-                                        Ok(msg) => display(&msg),
-                                        Err(e) => eprintln!("Message decode failed: {}", e),
-                                    };
-
-                                    pos += m;
-                                    cobs_buf = vec![0; 2048];
-                                    cobs_dec = CobsDecoder::new(cobs_buf.as_mut_slice());
-                                }
-                                Ok(None) => break 'inner,
-                                Err(e) => {
-                                    // TODO: log levels to see errors. These usually happen at the start,
-                                    //   when we have received a bad partial message fragment
-                                    eprintln!("Warning: Cobs decoding failed at byte: {}", pos + e);
-                                    cobs_buf = vec![0; 2048];
-                                    cobs_dec = CobsDecoder::new(cobs_buf.as_mut_slice());
-                                    break 'inner;
-                                }
-                            }
-                        }
-
-                    },
-                    Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
-                    Err(e) => {
-                        eprintln!("{:?}", e);
-                        ::std::process::exit(1);
-                    }
-                }
-            }
+        Ok(port) => {
+            modem_task(port, prod_cmds).unwrap();
         }
         Err(e) => {
             eprintln!("Failed to open \"{}\". Error: {}", "/dev/ttyACM0", e);
             ::std::process::exit(1);
         }
     }
-}
-
-fn display(msg: &LogOnLine<ModemUartMessages>) {
-    match msg {
-        LogOnLine::Log(m) => {
-            println!("{}", prefixed_lines(m, "LOG"))
-        }
-        _ => {}
-    }
-}
-
-use chrono::prelude::*;
-
-fn prefixed_lines(st: &str, msg: &str) -> String {
-    let mut out = String::new();
-    out += &format!("{:?}\n", Local::now());
-    st.lines().for_each(|line| {
-        out += &format!(
-            " => {}: {}\n",
-            msg,
-            line
-        );
-    });
-    out.truncate(out.trim_end().len());
-    out
 }
---
+++ beginner/draw-server/src/modem_comms.rs
@@ -0,0 +1,104 @@
+use serialport::prelude::*;
+use postcard::from_bytes;
+use nrf52_bin_logger::LogOnLine;
+use cobs::decode_vec;
+use protocol::{ModemUartMessages, CellCommand};
+use std::sync::mpsc::Sender;
+
+struct Modem {
+    port: Box<dyn SerialPort>,
+    cobs_buf: Vec<u8>,
+    // cobs_dec: CobsDecoder<'static>,
+}
+
+impl Modem {
+    fn process_serial(&mut self) -> Result<Vec<ModemUartMessages>, ()> {
+        let mut buf = [0u8; 1024];
+        let buf = match self.port.read(&mut buf) {
+            Ok(ct) => &buf[..ct],
+            Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => return Ok(vec![]),
+            Err(e) => {
+                eprintln!("{:?}", e);
+                return Err(());
+            }
+        };
+
+        self.push_bytes(buf)
+    }
+
+    fn push_bytes(&mut self, mut data: &[u8]) -> Result<Vec<ModemUartMessages>, ()> {
+        let mut resps = vec![];
+
+        while let Some(idx) = data.iter().position(|&b| b == 0) {
+            let (end, rest) = data.split_at(idx+1);
+            self.cobs_buf.extend_from_slice(end);
+
+            use LogOnLine::*;
+            if let Ok(idx) = cobs::decode_in_place(&mut self.cobs_buf) {
+                match from_bytes::<LogOnLine<ModemUartMessages>>(&self.cobs_buf[..idx]) {
+                    Ok(ProtocolMessage(desmsg)) =>  {
+                        resps.push(desmsg);
+                    }
+                    Ok(other) => display(&other),
+                    Err(e) => {
+                        eprintln!("bad_decode: {:?}", e);
+                    }
+                }
+            }
+
+            data = rest;
+            self.cobs_buf.clear();
+        }
+
+        self.cobs_buf.extend_from_slice(data);
+
+        Ok(resps)
+    }
+}
+
+pub fn modem_task(port: Box<dyn SerialPort>, _prod_cmds: Sender<CellCommand>) -> Result<(), ()> {
+    println!("Receiving data on {} at {} baud:", "/dev/ttyACM0", "115200");
+
+    let mut modem = Modem {
+        port,
+        cobs_buf: vec![],
+    };
+
+    loop {
+        let _msgs = modem.process_serial();
+    }
+}
+
+fn display(msg: &LogOnLine<ModemUartMessages>) {
+    match msg {
+        LogOnLine::Log(log) => {
+            eprintln!("{}", prefixed_lines(log, "LOG"));
+        }
+        LogOnLine::Warn(log) => {
+            eprintln!("{}", prefixed_lines(log, "WRN"));
+        }
+        LogOnLine::Error(log) => {
+            eprintln!("{}", prefixed_lines(log, "ERR"));
+        }
+        LogOnLine::BinaryRaw(log) => {
+            eprintln!("{}", prefixed_lines(&format!("{:02X?}", log), "BIN"));
+        }
+        _ => {}
+    }
+}
+
+use chrono::prelude::*;
+
+fn prefixed_lines(st: &str, msg: &str) -> String {
+    let mut out = String::new();
+    out += &format!("{:?}\n", Local::now());
+    st.lines().for_each(|line| {
+        out += &format!(
+            " => {}: {}\n",
+            msg,
+            line
+        );
+    });
+    out.truncate(out.trim_end().len());
+    out
+}
