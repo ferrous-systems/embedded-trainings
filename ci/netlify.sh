71029b8cb62fe92f6e87890027a3943804cbd6cc
James Munns <james.munns@ferrous-systems.com>
2019-04-24 10:59:23 +0200

    Cargo workspace to reduce rebuild time

--- ci/netlify.sh
+++ ci/netlify.sh
@@ -4,16 +4,9 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-t
 
 export PATH=$PATH:$HOME/.cargo/bin
 
-mkdir -p ./netlify
+mkdir -p ./netlify/beginner
 
-cd beginner/templates/segment-1
-cargo doc --target x86_64-unknown-linux-gnu
-cd ../segment-3
-cargo doc --target x86_64-unknown-linux-gnu
-cd ../segment-4
-cargo doc --target x86_64-unknown-linux-gnu
-cd ..
+cd beginner/templates/
+cargo doc --all --target x86_64-unknown-linux-gnu
 
-mv ./segment-1/target/x86_64-unknown-linux-gnu/doc ../../netlify/segment-1-docs
-mv ./segment-3/target/x86_64-unknown-linux-gnu/doc ../../netlify/segment-3-docs
-mv ./segment-4/target/x86_64-unknown-linux-gnu/doc ../../netlify/segment-4-docs
+mv ./target/x86_64-unknown-linux-gnu/doc/ ../../netlify/beginner
